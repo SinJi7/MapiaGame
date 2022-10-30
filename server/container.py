f79972b2b5f7e28ff9bd5ead8d10d4ec27bcc6f2
Jin <jineun300@gmail.com>
2022-10-30 21:35:39 +0900

    add container.py

---
+++ server/container.py
@@ -0,0 +1,42 @@
+import threading
+
+
+class Container(threading.Thread):
+    """클래스 생성시 threading.Thread를 상속받아 만들면 된다"""
+    __RoomValid = True
+    __GamePlay = False
+    
+    def __init__(self, RoomID, Users:list):
+        """__init__ 메소드 안에서 threading.Thread를 init한다"""
+        threading.Thread.__init__(self)
+        #add Use Mapia-Core
+        self.__Users = []
+        self.__RoomID = RoomID
+    
+    def isRoomId(self, ID):
+        return True if ID == self.__RoomID else False
+
+    #False일 경우 객체 제거
+    def isRoomValid(self):
+        return self.__RoomValid
+
+    def __doGame():
+        pass #게임 진행 함수 여기에
+        #reset 조건 필요
+
+    #start 실행
+    def run(self):
+        while self.__RoomValid:
+            if self.__GamePlay: self.__doGame()
+
+            #채팅 부분 여기에 구현해야함
+
+
+
+    
+def make_container_start(RoomNumber, User):
+    container = Container()
+    container.start()
+    return container
+    
+
--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
+from flask import jsonify
+import secrets
 
 from flask_cors import CORS, cross_origin
 
@@ -11,12 +13,23 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 def sessions():
     return render_template('session.html')
 
+@app.route('/token', methods=["GET"])
+def getToken():
+    room_number=request.args.get('room_number')
+    rand_token=secrets.token_hex(nbytes=16)
+    return jsonify(
+        token=rand_token
+    )
+
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
 @socketio.on('message')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
+    
+
+    #이벤트 처리 추가
     #socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
